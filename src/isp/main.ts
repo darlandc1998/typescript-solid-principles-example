ecbec402d0d48e71112fa158375fcd986d0a86d8
Darlan <darlan.candiotto98@gmail.com>
2023-01-04 17:20:43 -0300

    Implements isp

---
+++ src/isp/main.ts
@@ -0,0 +1,36 @@
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+import { IndividualCustomer } from './classes/customer';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+//const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount); //noDiscount, tenPercentDiscount or fiftyPercentDiscount
+const messaging = new Messaging();
+const persistency = new Persistency();
+const individualCustomer = new IndividualCustomer(
+  'Luiz',
+  'Miranda',
+  '111.111.111-11',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  individualCustomer,
+);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
